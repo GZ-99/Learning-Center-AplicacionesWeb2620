57fdb63d29fad062a5ce3a444d7e9c352568d673
GZ-99 <gabrielvilchezvite@gmail.com>
2026-09-23 09:19:26 -0500

    chore: second commit.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import LanguageSwitcher from "@/shared/presentation/components/language-switcher.vue";
 </script>
 
 <template>
-
+  <language-switcher />
+  <p>{{ $t('language')}}</p>
 </template>
 
 <style scoped>
---
+++ src/publishing/domain/model/tutorial.entity.js
@@ -0,0 +1,13 @@
+import {Category} from "@/publishing/domain/model/category.entity.js";
+
+export class Tutorial {
+    constructor({id = null, title = '', summary = '',
+                categoryId = '', category = null} = {}) {
+        this.id = id;
+        this.title = title;
+        this.summary = summary;
+        this.categoryId = categoryId;
+        this.category = category instanceof Category
+            ? category : null;
+    }
+}
